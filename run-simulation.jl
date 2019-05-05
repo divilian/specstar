118ec417d563db51f119d423cbfb6877a459d0b3
srikanthmudigonda <47902184+srikanthmudigonda@users.noreply.github.com>
2019-05-05 18:21:54 -0500

    Add files via upload
    Added code to implement 
    * institutions
    * inbound/outbound migration + births
    * execute multiple simulation runs using an arbitrarily-chosen set of 10 seeds

--- run-simulation.jl
+++ run-simulation.jl
@@ -1,5 +1,8 @@
 include("Sugarscape.jl")
 include("Agent.jl")
+include("Institution.jl")
+include("max-num-generator.jl")
+
 using Statistics
 using Random
 using Distributions
@@ -47,41 +50,66 @@ function set_up_environment(scape_side, scape_carry_cap, scape_growth_rate,
     for loc in arr_poss_locations
         sugscape_obj[loc[1], loc[2]].occupied = true
     end
-    
+    # println("Created a sugarscape of size: ", 
+    #         string(size(sugscape_obj)[1] * size(sugscape_obj)[2]))
+    # println("Created ", string(length(arr_agents)), " agents.")
     return(Dict("sugscape_obj" => sugscape_obj,
                 "arr_agents" => arr_agents)) 
 end ## end of set_up_environment()
 
-function animate_sim(sugscape_obj, arr_agents, time_periods)
+function animate_sim(sugscape_obj, arr_agents, time_periods, 
+                     birth_rate, inbound_rate, outbound_rate,
+                     vision_range_tpl, metab_range_tpl, suglvl_range_tpl,
+                     threshold)
     """
     Performs the various operations on the sugarscape and agent population
     to 'animate' them.
     Returns a single row, consisting of all of the params + gini values
-    of sugar across all the time periods
-    
+    of sugar across all the time periods.    
     """
-    arr_ginis = zeros(time_periods)
+    metabol_distrib =  DiscreteUniform(metab_range_tpl[1], metab_range_tpl[2]);
+    vision_distrib = DiscreteUniform(vision_range_tpl[1], vision_range_tpl[2]);
+    suglvl_distrib = DiscreteUniform(suglvl_range_tpl[1], suglvl_range_tpl[2]); 
+    arr_agent_ginis = zeros(time_periods)
+    ## the following is a hack because creating an empty array of Array{Institution, 1}
+    ## and adding Institution objects via push! is resulting in errors.
+    ## So to add type-checking on arr_institutions, we're going to initialize it with
+    ## a dummy Institution object
+    # arr_institutions = Array{Institution, 1}
+    arr_institutions = [Institution(-1, -1, false, [-1], [Transaction(-1, -1, "", -1)])]
+    
     for period in 1:time_periods 
         for ind in shuffle(1:length(arr_agents))
-            locate_move_feed!(arr_agents[ind], sugscape_obj)            
-        end
-        life_check!(arr_agents)
+            locate_move_feed!(arr_agents[ind], sugscape_obj, arr_agents, arr_institutions, period)
+        end 
         regenerate_sugar!(sugscape_obj)
-        ## println(get_sugarscape_stats(sugscape_obj))
-        gini_coeff = compute_Gini(arr_agents)
-        ## println("Here's the gini:",
-                # string(gini_coeff))
-        arr_ginis[period] = gini_coeff
-        ## println("Finished time-step: ", string(period), "\n\n")
-        
+        perform_birth_inbound_outbound!(arr_agents, sugscape_obj, birth_rate, 
+                                        inbound_rate, outbound_rate, 
+                                        vision_distrib, metabol_distrib,
+                                        suglvl_distrib) 
+        form_possible_institutions!(arr_agents, threshold, sugscape_obj, 
+                                    arr_institutions, period)
+
+        arr_agents = life_check!(arr_agents)
+        @assert all([aggobj.alive for aggobj in arr_agents])
+        # println("HERHEREHERE")
+        # readline()
+        update_occupied_status!(arr_agents, sugscape_obj)
+        update_institution_statuses!(arr_institutions, period)
+        arr_agent_ginis[period] = compute_Gini(arr_agents)
+        # println("No. of institutions: ", length(arr_institutions))
+        # println("No. of agents: ", string(length(arr_agents)))
+        # println("Finished time-step: ", string(period), "\n\n") 
+        ## println("Enter enter")
+        ## readline()
     end## end of time_periods for loop
-    return(arr_ginis)
+    return(arr_agent_ginis)
 end ## end animate_sim()
 
-function run_sim()
-    ## Random.seed!(13990);
+function run_sim(givenseed)
+    Random.seed!(givenseed)
     params_df = CSV.read("parameter-ranges-testing.csv")
-    outfile_name = "outputs.csv"
+    outfile_name = "outputs-new.csv"
     time_periods = 100
 
     temp_out = DataFrame(zeros(nrow(params_df), time_periods))
@@ -97,14 +125,19 @@ function run_sim()
     end
     
     for rownum in 1:nrow(params_df)
-        scape_side = params_df[2, :Side]
-        scape_carry_cap = params_df[2, :Capacity]
-        scape_growth_rate = params_df[2, :RegRate]
-        metab_range_tpl = (1, params_df[2, :MtblRate])
-        vision_range_tpl = (1, params_df[2, :VsnRng])
-        suglvl_range_tpl = (1, params_df[2, :InitSgLvl])
-        pop_density = params_df[2, :Adensity]
-
+        scape_side = params_df[rownum, :Side]
+        scape_carry_cap = params_df[rownum, :Capacity]
+        scape_growth_rate = params_df[rownum, :RegRate]
+        metab_range_tpl = (1, params_df[rownum, :MtblRate])
+        vision_range_tpl = (1, params_df[rownum, :VsnRng])
+        suglvl_range_tpl = (1, params_df[rownum, :InitSgLvl])
+        pop_density = params_df[rownum, :Adensity]
+        birth_rate = params_df[rownum, :Birthrate]
+        inbound_rate = params_df[rownum, :InbndRt]
+        outbound_rate = params_df[rownum, :OtbndRt]
+        threshold = params_df[rownum, :Threshold]
+        
+            
         dict_objs = set_up_environment(scape_side, scape_carry_cap,
                                        scape_growth_rate, pop_density,
                                        metab_range_tpl, vision_range_tpl,
@@ -119,14 +152,17 @@ function run_sim()
         ## next, animate the simulation - move the agents, have them consume sugar,
         ## reduce the sugar in sugscape cells, regrow the sugar....and collect the
         ## array of gini coeffs
-        arr_ginis = animate_sim(sugscape_obj, arr_agents, time_periods)
+        arr_agent_ginis = animate_sim(sugscape_obj, arr_agents, time_periods, 
+                                      birth_rate, inbound_rate, outbound_rate,
+                                      vision_range_tpl, metab_range_tpl, 
+                                      suglvl_range_tpl, threshold)
 
         # for colname in names(params_df)
         #     out_df[rownum, Symbol(colname)] = params_df[rownum, Symbol(colname)]
         # end
 
         for colnum in ncol(params_df)+1 : ncol(out_df)
-            out_df[rownum, colnum] = arr_ginis[colnum - ncol(params_df)]
+            out_df[rownum, colnum] = arr_agent_ginis[colnum - ncol(params_df)]
         end
         
         
@@ -136,10 +172,13 @@ function run_sim()
         # println(out_df)
         # readline()
     end #end iterate over param rows 
-    
-    ## return the output df 
-    return(out_df)
-    
+
+    return(out_df)    
 end ## run_sim
 
-run_sim() |> CSV.write("output.csv")
+# run_sim() |> CSV.write("output.csv")
+arr_seeds = [10, 80085, 4545, 4543543535, 87787765, 63542, 34983, 596895, 2152, 434];
+for seednum in arr_seeds
+    fname = "outputfile-" * string(seednum) * ".csv"
+    run_sim(seednum) |> CSV.write(fname)
+end
