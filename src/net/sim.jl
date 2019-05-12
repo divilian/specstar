e53b4906b68db8ba0f354545b43d3748212d5c65
Will Nehrboss <wknehrboss@gmail.com>
2019-05-11 20:37:13 -0400

    Fix #14 ER SW SF models

--- src/net/sim.jl
+++ src/net/sim.jl
@@ -149,6 +149,23 @@ function specnet(params)
                         colorant"pink" : colorant"lightgrey")
         for node in 1:nv(graph) ]
     end
+	function chose_graph()
+	    if whichGraph=="erdos_renyi"
+	        graph = LightGraphs.SimpleGraphs.erdos_renyi(N,ER_prob)
+            println("Erdos renyi...")
+		end 
+	 
+	    if whichGraph=="scale_free"
+	        graph = LightGraphs.SimpleGraphs.static_scale_free(N, SF_edges, SF_degree)
+            println("Scale free...")
+		end
+	
+	    if whichGraph=="small_world"
+	        graph = LightGraphs.SimpleGraphs.watts_strogatz(N,SW_degree, SF_prob)
+            println("Small world...")
+		end
+	return graph
+	end
    ginis=[]
     rev_dict(d) = Dict(y=>x for (x,y) in d)
     ###########################################################################
@@ -161,14 +178,19 @@ function specnet(params)
     # agent numbers. (Could be a set instead of a list, but we're using it as
     # an index to the colors array, to uniquely color members of each proto.)
     global protos = Set{Char}[]
- 
+ if(chose_graph=="small_world")
+	 println("hello")
+ end
+ println("hellol")
     # The numbers of agents who have perished (initially none).
     global dead = Set{Char}()
 
     # The initial social network.
-    global graph = LightGraphs.SimpleGraphs.erdos_renyi(N,.2)
+    global graph = chose_graph()
+	
     while !is_connected(graph)
-        global graph
+        global graph = chose_graph()
+
         pri("Not connected; regenerating...")
         graph = LightGraphs.SimpleGraphs.erdos_renyi(N,.2)
     end
