6621be233dfffa91c3d5e2d6af0536306cf59a4d
Stephen <stephen@umw.edu>
2019-05-17 17:42:04 -0400

    Add net version of fetch_eligible_neighbors().

--- src/net/NetAgents.jl
+++ src/net/NetAgents.jl
@@ -1,6 +1,8 @@
 include("../star/StarAgents.jl")
 include("../star/Protos.jl")
 
+using LightGraphs
+
 mutable struct NetAgent
     a::StarAgent
 
@@ -19,3 +21,17 @@ end
 function Base.show(io::IO, na::NetAgent)
     show(io, na.a)
 end
+
+
+function fetch_eligible_neighbors(agobj, arr_agents,
+    specnet_graph::SimpleGraph{Int64})
+
+    """
+    For a given agent, identify agents that have a connection to it and
+    whose sugar_level > proto_threshold and return them.
+    """
+
+    return shuffle([n for n in neighbors(specnet_graph, AN[agobj])
+        if n.a.sugar_level > params[:proto_threshold]])
+end ## end fetch_eligible_neighbors
+
