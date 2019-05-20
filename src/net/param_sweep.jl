071bc2efd55fd64c6024e9c9ec765085701edf82
Stephen <stephen@umw.edu>
2019-05-20 14:52:23 -0400

    Create setup_params.jl.

--- src/net/param_sweep.jl
+++ src/net/param_sweep.jl
@@ -1,61 +1,63 @@
 #!/usr/bin/env julia
 using Revise
-using RCall 
+using RCall
 @rlibrary ineq
 using DataFrames
 using CSV
 include("sim.jl")
-include("run-simulation.jl")
+include("setup_params.jl")
 
 
 param_to_sweep=:salary_range    #parameter to iterate over
 start_value=10                  #value to begin sweep
-end_value=210                   #value to end sweep 
+end_value=210                   #value to end sweep
 graph_sweep=false               #run sweep for each graph type
 num_steps=100
 
-    
- 
+
+
 function param_sweeper(graph_name)
     counter=start_value
     large_df=DataFrame(agent=String[],sugar=Float64[], proto_id=Int[], counter_value=Float64[])
 
+    params[:make_anims] = false  # We would never want this true for a sweep 
+
     for i= 1:num_steps
-        
+
         if typeof(params[param_to_sweep])==Int64
-            
+
             param_counter=convert(Int64,floor(counter))
-        
-        
+
+
         else
             param_counter=counter
         end
-        
+
         params[param_to_sweep]=param_counter
         results=specnet()
         insert!(results,4,repeat(counter:counter,nrow(results)),:counter_value)
         large_df=[large_df;results]
-         counter+=(( end_value-  start_value)/num_steps)
-        
+        counter+=(( end_value-  start_value)/num_steps)
+
     end
     rm("$(tempdir())/$(graph_name)ParameterSweepDataFrame.csv", force=true)
     rm("$(tempdir())/$(graph_name)ParameterSweepPlot.png", force=true)
     CSV.write("$(tempdir())/$(graph_name)ParameterSweep.csv",large_df)
 
     #once the main dataframe is made, plots may be drawn with data from large_df
-    #plotting and construction of dataframe is in separate for loop for optimal organiztion 
+    #plotting and construction of dataframe is in separate for loop for optimal organiztion
 
     #dataframe containing only values to be plotted
-    plot_df=DataFrame(gini=Float64[],counter_value=Float64[])  #change values of this dataframe 
+    plot_df=DataFrame(gini=Float64[],counter_value=Float64[])  #change values of this dataframe
                                                                #to create other plots
     #loop to populate dataframe
 
-     counter=start_value
+    counter=start_value
     for j = 1:num_steps
-     counter+=((end_value-start_value)/num_steps)
+        counter+=((end_value-start_value)/num_steps)
 
         #change this code to populate above dataframe with alt data
-        push!(plot_df, (ineq((large_df[large_df.counter_value.==counter, :sugar]), "Gini"),counter)) 
+        push!(plot_df, (ineq((large_df[large_df.counter_value.==counter, :sugar]), "Gini"),counter))
     end
 
     #drawing plot
@@ -69,11 +71,11 @@ end
 
 if graph_sweep
     graphs=["erdos_renyi","scale_free", "small_world"]
-                                    
+
     for k in 1:3
         params[:whichGraph]=graphs[k]
         param_sweeper(string(graphs[k]))
-            
+
     end
 else
     param_sweeper(params[:whichGraph])
---
+++ src/net/setup_params.jl
@@ -0,0 +1,32 @@
+
+## Input parameters
+params = Dict{Symbol,Any}(
+    :N => 30,                       # number of agents
+    :num_iter => 50,                # number of iterations the simulation runs
+    :openness => 0.0,               # 0 <=> openness <=> 1
+                                    #   (0: always choose from neighbor,
+                                    #    1: always choose from entire city)
+    :max_starting_wealth => 100,    # each agent starts with wealth
+                                    #   U~(0,max_starting_wealth)
+    :salary_range => 10,            # each iteration, each agent receives/loses
+                                    #   U~(-salary_range, salary_range) wealth
+    :proto_threshold => 50,         # each agent in an encounter must have
+                                    #   > wealth than this to form a proto
+    :make_anims => false,           # create animations of results?
+    :animation_delay => 20,         # milliseconds between animation frames
+    :random_seed => 1234,           # random number generator starting seed
+
+    :whichGraph=>"erdos_renyi",     # the name of graph to generate e.g.
+                                    #   - "erdos_renyi"
+                                    #   - "scale_free"
+                                    #   - "small_world"
+
+    :ER_prob=>0.2,                  # probability of edge between two vertices
+
+    :SF_edges=>40,                  # number of edges in SF model
+    :SF_degree=>2,                  # exponent of expected power law degree
+                                    #   distribution
+
+    :SW_degree=>4,                  # degree (even integer) number of neighbors 
+    :SW_prob=>0.2,                  # probability of rewiring (between 0/1)
+)
