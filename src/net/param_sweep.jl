a484e4c616b678a76820ba13ef6eba390397d873
Will Nehrboss <wknehrboss@gmail.com>
2019-05-20 16:40:52 -0400

    Param sweep iterations

--- src/net/param_sweep.jl
+++ src/net/param_sweep.jl
@@ -13,33 +13,35 @@ start_value=10                  #value to begin sweep
 end_value=210                   #value to end sweep
 graph_sweep=false               #run sweep for each graph type
 num_steps=100
-
+iter_per_step=50
 
 
 function param_sweeper(graph_name)
     counter=start_value
-    large_df=DataFrame(agent=String[],sugar=Float64[], proto_id=Int[], counter_value=Float64[])
+    large_df=DataFrame(agent=String[],sugar=Float64[], proto_id=Int[], counter_value=Float64[],iter_num_sweep=Int64[])
 
     params[:make_anims] = false  # We would never want this true for a sweep 
 
     for i= 1:num_steps
+        for j = 1:iter_per_step
+			if typeof(params[param_to_sweep])==Int64
 
-        if typeof(params[param_to_sweep])==Int64
-
-            param_counter=convert(Int64,floor(counter))
+				param_counter=convert(Int64,floor(counter))
 
 
-        else
-            param_counter=counter
+			else
+				param_counter=counter
+			end
+
+			params[param_to_sweep]=param_counter
+			results=specnet()
+			insert!(results,4,repeat(counter:counter,nrow(results)),:counter_value)
+			insert!(results,5,repeat((i*iter_per_step + j):(i*iter_per_step + j),nrow(results)),:iter_num_sweep)
+			large_df=[large_df;results]
+			
         end
-
-        params[param_to_sweep]=param_counter
-        results=specnet()
-        insert!(results,4,repeat(counter:counter,nrow(results)),:counter_value)
-        large_df=[large_df;results]
         counter+=(( end_value-  start_value)/num_steps)
-
-    end
+	end
     rm("$(tempdir())/$(graph_name)ParameterSweepDataFrame.csv", force=true)
     rm("$(tempdir())/$(graph_name)ParameterSweepPlot.png", force=true)
     CSV.write("$(tempdir())/$(graph_name)ParameterSweep.csv",large_df)
@@ -54,11 +56,15 @@ function param_sweeper(graph_name)
 
     counter=start_value
     for j = 1:num_steps
-        counter+=((end_value-start_value)/num_steps)
-
-        #change this code to populate above dataframe with alt data
-        push!(plot_df, (ineq((large_df[large_df.counter_value.==counter, :sugar]), "Gini"),counter))
-    end
+		total_gini=0
+		for i=1:iter_per_step
+		total_gini+=convert(Float64,ineq((large_df[large_df.iter_num_sweep.==(j*iter_per_step+i), :sugar]), "Gini"))
+        end
+		
+		#change this code to populate above dataframe with alt data
+        push!(plot_df, (total_gini/iter_per_step,counter))
+		counter+=((end_value-start_value)/num_steps)
+   end
 
     #drawing plot
     plotLG=plot(x=plot_df.counter_value,y=plot_df.gini, Geom.point, Geom.line,
